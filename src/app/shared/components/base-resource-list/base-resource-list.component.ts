00673e22c47fd17a22c4a4076059f38a1edd3bb3
Daniel Garcez <nysolution9@gmail.com>
2018-12-26 21:12:56 -0200

    Refatorando os componentes de listagem

---
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -0,0 +1,29 @@
+import { OnInit } from '@angular/core';
+
+import { BaseResourceModel } from '../../models/base-resources.model';
+import { BaseResourceService } from '../../services/base-resources.service';
+
+export abstract class BaseResourceListComponent<T extends BaseResourceModel> implements OnInit {
+
+  resources: T[] = []
+
+  constructor(private resourceService: BaseResourceService<T> ) { }
+
+  ngOnInit() {
+    this.resourceService.getAll().subscribe(
+      resources => this.resources = resources.sort((a,b)=> b.id - a.id),
+      error => alert("Erro ao carregar a listagem")
+    )
+  }
+
+  deleteResource(resource) {
+    const mustDelete = confirm("Deseja realmente excluir este item?")
+    if (mustDelete) {
+      this.resourceService.delete(resource.id).subscribe(
+        () => this.resources = this.resources.filter(element => element != resource),
+        () => alert("Erro ao tentar excluir o item")
+      ) 
+    }
+  }
+  
+}
